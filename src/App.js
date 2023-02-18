4c8fbc785d8c3898a580d3d3e132939ef9a22065
KELLSPING\kells <kellsping@gmail.com>
2023-02-18 16:37:11 +0800

    Update. Complete the view of the calculator.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,51 @@
+import { useState } from "react";
+import { NumericFormat } from "react-number-format";
 import "./App.css";
 
 function App() {
+    const [preState, setPreState] = useState("");
+    const [currState, setCurrState] = useState("");
+    const [input, setInput] = useState("0");
+    const [operator, setOperator] = useState(null);
+    const [total, setTotal] = useState(false);
+
     return (
         <div className="container">
             <div className="wrapper">
                 <div className="screen">
-                    
+                    {input !== "" || input === "0" ? (
+                        <NumericFormat
+                            value={input}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                        />
+                    ) : (
+                        <NumericFormat
+                            value={preState}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                        />
+                    )}
                 </div>
-                <div className="btn light-gray">
-                    AC
-                </div>
-                <div className="btn light-gray">
-                    %
-                </div>
-                <div className="btn light-gray">
-                    +/-
-                </div>
-                <div className="btn orange">
-                    /
-                </div>
-                <div className="btn">
-                    7
-                </div>
-                <div className="btn">
-                    8
-                </div>
-                <div className="btn">
-                    9
-                </div>
-                <div className="btn orange">
-                    X
-                </div>
-                <div className="btn">
-                    4
-                </div>
-                <div className="btn">
-                    5
-                </div>
-                <div className="btn">
-                    6
-                </div>
-                <div className="btn orange">
-                    +
-                </div>
-                <div className="btn">
-                    1
-                </div>
-                <div className="btn">
-                    2
-                </div>
-                <div className="btn">
-                    3
-                </div>
-                <div className="btn orange">
-                    -
-                </div>
-                <div className="btn zero">
-                    0
-                </div>
-                <div className="btn">
-                    .
-                </div>
-                <div className="btn">
-                    =
-                </div>
+                <div className="btn light-gray">AC</div>
+                <div className="btn light-gray">%</div>
+                <div className="btn light-gray">+/-</div>
+                <div className="btn orange">/</div>
+                <div className="btn">7</div>
+                <div className="btn">8</div>
+                <div className="btn">9</div>
+                <div className="btn orange">X</div>
+                <div className="btn">4</div>
+                <div className="btn">5</div>
+                <div className="btn">6</div>
+                <div className="btn orange">+</div>
+                <div className="btn">1</div>
+                <div className="btn">2</div>
+                <div className="btn">3</div>
+                <div className="btn orange">-</div>
+                <div className="btn">0</div>
+                <div className="btn">.</div>
+                <div className="btn equal">=</div>
             </div>
         </div>
     );
